df34275074e8a285983b1ebe72a75b7a26265703
ffati <alnaserfatimah344@gmail.com>
2023-04-11 05:09:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/swelab4/StudentMod.java
@@ -0,0 +1,46 @@
+package com.example.swelab4;
+
+public class StudentMod {
+    private int id;
+    private int age;
+    private String name;
+
+    public StudentMod(int id, String name, int age) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentMod{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
